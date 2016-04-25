ec63ca0b617c270b9fb54413753e5707bf16ec10
Art Garm <artem.garmatyk@gmail.com>
2016-04-25 13:56:51 +0300

    add

--- src/js/develop_2.js
+++ src/js/develop_2.js
@@ -2,24 +2,56 @@
 
 
 $(document).ready(function () {
-    $("input").focusin(function () { 
+
+    $('.spec-border').css("border-color",$('.mdl-color--accent').css('background-color') );
+    
+    $("input").focusin(function () {
         $(this).closest('.form_input').find('.super-label').addClass('translator');
     });
-    $("input").focusout(function () { 
-        if ( $(this).val() == '' || $(this).val().indexOf("_") !=-1 ) {
+    $("input").focusout(function () {
+        if ($(this).val() == '' || $(this).val().indexOf("_") != -1) {
             $(this).closest('.form_input').find('.super-label').removeClass('translator');
-        }        
+        }
     });
 
-    $('.call-phone').click(function () { 
-        $(this).closest('.super-button').find('.after-frorm').addClass('active-form');
+    $(document).on('click touchstart', function (event) {
+        if ($('.after-frorm').hasClass('active-form') && !$('.after-frorm ').is(event.target) && $('.after-frorm ').has(event.target).length === 0 && !$('.call-phone').is(event.target) && $('.call-phone').has(event.target).length === 0) {
+            $('.header').removeClass('hide-hipe');
+            $('.main').removeClass('hide-hipe');
+            $('.after-frorm').removeClass('active-form');
+        }
     });
+
+    $('.call-phone').click(function () {
+        if ($('.after-frorm').hasClass('active-form')) {
+            $('.header').removeClass('hide-hipe');
+            $('.main').removeClass('hide-hipe');
+            $('.after-frorm').removeClass('active-form');
+        } else {
+            $(this).closest('.super-button').find('.after-frorm').addClass('active-form');
+            $('.header').addClass('hide-hipe');
+            $('.main').addClass('hide-hipe');
+        }
+    });
+
+    $('form').find('button').prop("disabled", true);
+    $('form').find('button').addClass('doesnotuse');
+
+    $('input').on('change', function () {
+        if ($(this).val() != '' || $(this).val().indexOf("_") != -1) {
+            $(this).closest('form').find('button').prop("disabled", false);
+            $(this).closest('form').find('button').removeClass('doesnotuse');
+        }
+    });
+    
+
+
 });
 
-$(window).load(function(){
+$(window).load(function () {
 
 });
 
-$(window).resize(function(){
+$(window).resize(function () {
 
 });
