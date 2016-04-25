1d62b046ebd026188967d9cf63a764ed2c93f2a9
Art Garm <artem.garmatyk@gmail.com>
2016-04-25 11:10:55 +0300

    add

--- src/js/develop_2.js
+++ src/js/develop_2.js
@@ -1,8 +1,20 @@
 
 
 
-$(document).ready(function(){
-
+$(document).ready(function () {
+    /*
+    $('.super-label').click(function () {
+        $(this).addClass('translator');
+    });
+*/
+    $("input").focusin(function () { 
+        $(this).closest('.form_input').find('.super-label').addClass('translator');
+    });
+    $("input").focusout(function () { 
+        if ( $(this).val() == '' || $(this).val().indexOf("_") !=-1 ) {
+            $(this).closest('.form_input').find('.super-label').removeClass('translator');
+        }        
+    });
 });
 
 $(window).load(function(){
